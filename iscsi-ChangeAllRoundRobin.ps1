a1838baff657fde3760861aa8287f365724af7a8
Jason <jtlivingston@usgs.gov>
2017-06-21 21:23:53 -0600

    iscsi-ChangeAllRoundRobin
    This will change all LUNs multipathing policies from anything not Round Robin and make them Round Robin.

---
+++ iscsi-ChangeAllRoundRobin.ps1
@@ -0,0 +1,6 @@
+﻿<#
+    Get the iscsi LUN's that are not set to Round Robin for pathing policy and then change
+    policy to use Round Robin on every LUN .
+#>
+Get-VMHost | Get-ScsiLun -LunType disk | Where {$_.MultipathPolicy -notlike "RoundRobin"} | 
+Where {$_.CapacityGB -ge 100} | Set-Scsilun -MultiPathPolicy RoundRobin
