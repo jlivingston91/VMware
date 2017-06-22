c436178560e0013e42d527e8827a8c9f2439f313
Jason <jtlivingston@usgs.gov>
2017-06-21 22:08:42 -0600

    network-NotVMxnet
    This will find anything in vcenter that does not have vmxnet3 adapter installed and output to screen or optional csv file

---
+++ network-NotVMxnet3.ps1
@@ -0,0 +1,10 @@
+﻿<#
+    This script will retreive all of the network adapters and ouput (optionally)
+    to a file of your choosing.
+#>
+Get-VM | Get-NetworkAdapter | 
+Where-object {$_.Type -ne "Vmxnet3"} | 
+Select @{N="VM";E={$_.Parent.Name}},Name,Type
+
+#Remove the line to export to a csv file, otherwise it will print to screen
+ #| export-Csv  c:\Network_Interface.csv -NoTypeInformation
